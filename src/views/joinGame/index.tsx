696725aa537aceedc479936033749a5557fe8943
cryptoloutre <miningcryptomonnaie@gmail.com>
2024-10-02 19:03:56 +0200

    fix join game message not displayed

--- src/views/joinGame/index.tsx
+++ src/views/joinGame/index.tsx
@@ -13,7 +13,8 @@ import { shortAddress } from "utils/shortAddress";
 export const JoinGameView: FC = ({ }) => {
   const { publicKey } = useWallet();
   const program = new Program(IDL, PROGRAM_ID, { connection });
-  const [myGames, setMyGames] = useState<{ id: number, players: string[], game: string, winner: string }[]>(null);
+  const [myOngoingGames, setMyOngoingGames] = useState<{ id: number, players: string[], game: string, winner: string }[]>([]);
+  const [myFinishedGames, setMyFinishedGames] = useState<{ id: number, players: string[], game: string, winner: string }[]>([]);
   const [gamesFetched, setGamesFetched] = useState<boolean>(false);
   const [gameAddress, setGameAddress] = useState<String>('');
 
@@ -28,7 +29,8 @@ export const JoinGameView: FC = ({ }) => {
     // @ts-ignore
     const allGames = await program.account.gameMaster.all();
 
-    const userGames = [];
+    const userOngoingGames = [];
+    const userFinishedGames = [];
 
     allGames.map((game: any) => {
       const _players = game.account.players;
@@ -38,23 +40,42 @@ export const JoinGameView: FC = ({ }) => {
           for (let i = 0; i < _players.length; i++) {
             players.push(_players[i].toBase58())
           }
-          userGames.push({
-            id: Number(game.account.id),
-            game: game.publicKey.toBase58(),
-            players: players,
-            winner: game.account.winner.toBase58()
-          })
+          const winner = game.account.winner.toBase58();
+          if (winner == PublicKey.default.toBase58()) {
+            userOngoingGames.push({
+              id: Number(game.account.id),
+              game: game.publicKey.toBase58(),
+              players: players,
+              winner: game.account.winner.toBase58()
+            })
+          }
+          else {
+            userFinishedGames.push({
+              id: Number(game.account.id),
+              game: game.publicKey.toBase58(),
+              players: players,
+              winner: game.account.winner.toBase58()
+            })
+          }
+
         }
       })
     })
-    userGames.sort(function (a, b) {
+    userOngoingGames.sort(function (a, b) {
       if (a.id < b.id) {
         return -1
       }
     })
-    setMyGames(userGames);
+    userFinishedGames.sort(function (a, b) {
+      if (a.id < b.id) {
+        return -1
+      }
+    })
+    setMyOngoingGames(userOngoingGames);
+    setMyFinishedGames(userFinishedGames);
     setGamesFetched(true);
-    console.log(userGames);
+    console.log(userOngoingGames);
+    console.log(userFinishedGames);
   }
 
   useEffect(() => {
@@ -75,73 +96,69 @@ export const JoinGameView: FC = ({ }) => {
 
           {!gamesFetched && publicKey && <div>Fetching...</div>}
 
-          {myGames.length == 0 && gamesFetched && <div>No game found</div>}
-
-          {myGames.length != 0 && gamesFetched && gameAddress == "" &&
-
+          <div>
             <div>
-              <div>
-                <div className="text-left text-2xl font-bold mb-2 underline">Ongoing Game</div>
-                <div className="grid justify-center gap-4 sm:grid-cols-2 md:max-w-[64rem] md:grid-cols-5">
-                  {myGames.map((game) =>
-                    game.winner == PublicKey.default.toBase58() &&
-                    (
-                      <Card key={game.id} className="border-secondary group-hover:border-white">
-                        <CardHeader>
-                          <CardTitle className="space-y-3">
-                            <span className="block font-bold group-hover:text-pretty">
-                              Game #{game.id}
-                            </span>
-                          </CardTitle>
-                        </CardHeader>
-                        <CardContent className="text-center">
-                          {game.players.map((player, key) => (
-                            <div key={key} className="">
-                              {shortAddress(player)}
-                              {key < game.players.length - 1 && <div className="flex justify-center"><SwordsIcon /></div>}
-                            </div>
-                          )
+              <div className="text-left text-2xl font-bold mb-2 underline">Ongoing Game</div>
+              <div className="grid justify-center gap-4 sm:grid-cols-2 md:max-w-[64rem] md:grid-cols-5">
+                {myOngoingGames.length != 0 ? myOngoingGames.map((game) =>
+                  game.winner == PublicKey.default.toBase58() &&
+                  (
+                    <Card key={game.id} className="border-secondary group-hover:border-white">
+                      <CardHeader>
+                        <CardTitle className="space-y-3">
+                          <span className="block font-bold group-hover:text-pretty">
+                            Game #{game.id}
+                          </span>
+                        </CardTitle>
+                      </CardHeader>
+                      <CardContent className="text-center">
+                        {game.players.map((player, key) => (
+                          <div key={key} className="">
+                            {shortAddress(player)}
+                            {key < game.players.length - 1 && <div className="flex justify-center"><SwordsIcon /></div>}
+                          </div>
+                        )
 
-                          )}
-                          <button onClick={() => setGameAddress(game.game)} className="mt-2 bg-[#312d29] border border-[#c8ab6e] py-1 px-3 rounded-lg uppercase font-bold">join</button>
-                        </CardContent>
-                      </Card>
-                    ))
-                  }
-                </div>
-              </div>
-              <div className="mt-4">
-                <div className="text-left text-2xl font-bold mb-2 underline">Game Finished</div>
-                <div className="grid justify-center gap-4 sm:grid-cols-2 md:max-w-[64rem] md:grid-cols-5">
-                  {myGames.map((game) =>
-                    game.winner != PublicKey.default.toBase58() &&
-                    (
-                      <Card key={game.id} className="border-secondary group-hover:border-white">
-                        <CardHeader>
-                          <CardTitle className="space-y-3">
-                            <span className="block font-bold group-hover:text-pretty">
-                              Game #{game.id}
-                            </span>
-                          </CardTitle>
-                        </CardHeader>
-                        <CardContent className="text-center">
-                          {game.players.map((player, key) => (
-                            <div key={key} className="">
-                              {shortAddress(player)}
-                              {key < game.players.length - 1 && <div className="flex justify-center"><SwordsIcon /></div>}
-                            </div>
-                          )
+                        )}
+                        <button onClick={() => setGameAddress(game.game)} className="mt-2 bg-[#312d29] border border-[#c8ab6e] py-1 px-3 rounded-lg uppercase font-bold">join</button>
+                      </CardContent>
+                    </Card>
+                  )) : <div>No game found...</div>}
 
-                          )}
-                          <button onClick={() => setGameAddress(game.game)} className="mt-2 bg-[#312d29] border border-[#c8ab6e] py-1 px-3 rounded-lg uppercase font-bold">join</button>
-                        </CardContent>
-                      </Card>
-                    ))
-                  }
-                </div>
               </div>
             </div>
-          }
+            <div className="mt-4">
+              <div className="text-left text-2xl font-bold mb-2 underline">Game Finished</div>
+              <div className="grid justify-center gap-4 sm:grid-cols-2 md:max-w-[64rem] md:grid-cols-5">
+                {myFinishedGames.length != 0 ? myFinishedGames.map((game) =>
+                  game.winner != PublicKey.default.toBase58() &&
+                  (
+                    <Card key={game.id} className="border-secondary group-hover:border-white">
+                      <CardHeader>
+                        <CardTitle className="space-y-3">
+                          <span className="block font-bold group-hover:text-pretty">
+                            Game #{game.id}
+                          </span>
+                        </CardTitle>
+                      </CardHeader>
+                      <CardContent className="text-center">
+                        {game.players.map((player, key) => (
+                          <div key={key} className="">
+                            {shortAddress(player)}
+                            {key < game.players.length - 1 && <div className="flex justify-center"><SwordsIcon /></div>}
+                          </div>
+                        )
+
+                        )}
+                        <button onClick={() => setGameAddress(game.game)} className="mt-2 bg-[#312d29] border border-[#c8ab6e] py-1 px-3 rounded-lg uppercase font-bold">join</button>
+                      </CardContent>
+                    </Card>
+                  ))
+                  : <div>No game found...</div>}
+              </div>
+            </div>
+          </div>
+
 
           {gameAddress != "" && <Game gameAddress={gameAddress} setGameAddress={setGameAddress} />}
         </div>
