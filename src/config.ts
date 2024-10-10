a4bcc13d7be56e9e726e2288097cd3c922ecf1a2
La Loutre <35653371+cryptoloutre@users.noreply.github.com>
2024-10-10 11:09:57 +0200

    change rpc endpoint

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ import { IDL } from "idl/idl";
 export const PROGRAM_ID: PublicKey = new PublicKey("5envej2Ue7gH2EnBHJsqCEyHrkVPUoyScVb5seHn4p8v");
 
 export const connection = new Connection(
-    "https://devnet.helius-rpc.com/?api-key=634713f0-b4f2-41dc-af7f-ed7d60bd70e2", "confirmed"
+    "https://devnet.helius-rpc.com/?api-key=fd8837b0-d184-46e2-9894-e9bde8a446be", "confirmed"
   );
 
 export const program = new Program(IDL, PROGRAM_ID, { connection });
